a3ee854dda26b3dbb3500ded7ef3ea51e4045c91
acoustic424 <acoustic424@gmail.com>
2021-08-23 02:19:08 +0900

    add login

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const port = 4000
 //const bodyParser = require('body-parser') body-parser is deprecated
 const { User } = require("./models/User")
 const config = require('./config/key')
+const cookieParser = require('cookie-parser')
 
 //application/x-www-form-urlencoded
 app.use(express.urlencoded({extended: true}))
 
 //application/json
 app.use(express.json())
+
+app.use(cookieParser())
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, { 
@@ -28,6 +31,31 @@ app.post('/register', (req, res) => {
     if (err) return res.json({ success: false, err})
     return res.status(200).json({
       success: true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user){
+      return req.json({
+        loginSuccess: false,
+        message: "Cannot find the user of email requested."
+      })
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch){
+        return res.json({ 
+          loginSuccess: false, 
+          message: "The password is not correct."
+        })
+      }
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+          res.cookie("x-auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      })
     })
   })
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const config = require('../config/key')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
   name: {
@@ -12,7 +16,7 @@ const userSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    maxLength: 50
+    maxLength: 60
   },
   role: {
     type: Number,
@@ -26,6 +30,43 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+userSchema.pre('save', function( next ){
+  // encoding password
+  var user = this //userSchema
+  if(user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if(err) return next(err)
+  
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if(err) return next(err)
+        user.password = hash
+        next() //return to user.save in index.js
+      })
+    })
+  } else {
+    next()
+  }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if(err) return callback(err)
+    callback(null, isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(callback) {
+  var user = this
+
+  var token = jwt.sign(user._id.toHexString(), config.secretToken)
+  user.token = token
+  user.save(function(err, user) {
+    if(err) return callback(err)
+    callback(null, user)
+  })
+}
+
 
 const User = mongoose.model('User', userSchema);
 
