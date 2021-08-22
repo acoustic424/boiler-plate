6e0570ca0846951cb564e20dfc16925c3c25ea57
acoustic424 <acoustic424@gmail.com>
2021-08-22 15:34:37 +0900

    add protect secret info

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 4000
+//const bodyParser = require('body-parser') body-parser is deprecated
+const { User } = require("./models/User")
+
+//application/x-www-form-urlencoded
+app.use(express.urlencoded({extended: true}))
+
+//application/json
+app.use(express.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://dy_node:12345@boilerplate.mt3b1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { 
@@ -9,7 +17,18 @@ mongoose.connect('mongodb+srv://dy_node:12345@boilerplate.mt3b1.mongodb.net/myFi
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World Node.js!')
+  res.send('Hello World Node.js! Welcome back!')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
