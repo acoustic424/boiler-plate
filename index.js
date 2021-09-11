ffafe9f38dbc1c6290dc8e86fad417b00eb29a97
acoustic424 <acoustic424@gmail.com>
2021-09-11 20:49:07 +0900

    add logout api

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
   res.send('Hello World Node.js! Welcome back!')
 })
 
-app.post('api/user/register', (req, res) => {
+app.post('/api/user/register', (req, res) => {
   const user = new User(req.body)
 
   user.save((err, userInfo) => {
@@ -36,7 +36,7 @@ app.post('api/user/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
   User.findOne({ email: req.body.email }, (err, user) => {
     if(!user){
       return req.json({
@@ -53,7 +53,7 @@ app.post('/login', (req, res) => {
       }
       user.generateToken((err, user) => {
         if(err) return res.status(400).send(err)
-          res.cookie("x-auth", user.token)
+          res.cookie("x_auth", user.token)
           .status(200)
           .json({ loginSuccess: true, userId: user._id })
       })
@@ -61,7 +61,7 @@ app.post('/login', (req, res) => {
   })
 })
 
-app.get('api/users/auth', auth, (req, res) => {
+app.get('/api/users/auth', auth, (req, res) => {
   res.status(200).json({
     _id: req.user._id,
     isAdmin: req.user.role === 0 ? false : true,
@@ -73,6 +73,18 @@ app.get('api/users/auth', auth, (req, res) => {
     image: req.user.image
   })
 })
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id }, 
+    { token: "" },
+    (err, user) => {
+      if(err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
